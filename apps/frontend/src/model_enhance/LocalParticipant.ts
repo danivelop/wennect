d1a2d7fd20dd3c3f1a754204c873ab1c1c2bf0c4
danivelop <mofg266@gmail.com>
2023-12-06 05:17:57 +0900

    refactor: 개선된 구조의LocalParticipant 모델 작성

---
+++ apps/frontend/src/model_enhance/LocalParticipant.ts
@@ -0,0 +1,230 @@
+import {
+  BehaviorSubject,
+  Subject,
+  from,
+  of,
+  forkJoin,
+  merge,
+  fromEvent,
+} from 'rxjs'
+import { map, tap, switchMap, filter, takeUntil, take } from 'rxjs/operators'
+
+interface TTrackNotifier {
+  mediaStream: MediaStream
+  track: MediaStreamTrack
+}
+
+class LocalParticipant {
+  private id: string
+
+  private mediaStreamList$: BehaviorSubject<MediaStream[]>
+
+  private trackList$: BehaviorSubject<MediaStreamTrack[]>
+
+  private addMediaStreamNotifier$: Subject<MediaStream>
+
+  private removeMediaStreamNotifier$: Subject<MediaStream>
+
+  private addTrackNotifier$: Subject<TTrackNotifier>
+
+  private removeTrackNotifier$: Subject<TTrackNotifier>
+
+  private enableTrackNotifier$: Subject<MediaStream>
+
+  constructor(id: string) {
+    this.id = id
+    this.mediaStreamList$ = new BehaviorSubject<MediaStream[]>([])
+    this.trackList$ = new BehaviorSubject<MediaStreamTrack[]>([])
+    this.addMediaStreamNotifier$ = new Subject<MediaStream>()
+    this.removeMediaStreamNotifier$ = new Subject<MediaStream>()
+    this.addTrackNotifier$ = new Subject<TTrackNotifier>()
+    this.removeTrackNotifier$ = new Subject<TTrackNotifier>()
+    this.enableTrackNotifier$ = new Subject<MediaStream>()
+
+    this.addMediaStreamNotifier$.subscribe((mediaStream) => {
+      this.mediaStreamList$.next([...this.mediaStreamList$.value, mediaStream])
+      mediaStream.getTracks().forEach((track) => {
+        this.addTrackNotifier$.next({ mediaStream, track })
+      })
+    })
+
+    this.removeMediaStreamNotifier$.subscribe((mediaStream) => {
+      this.mediaStreamList$.next(
+        this.mediaStreamList$.value.filter((ms) => ms.id !== mediaStream.id),
+      )
+      mediaStream.getTracks().forEach((track) => {
+        track.stop()
+        this.removeTrackNotifier$.next({ mediaStream, track })
+      })
+    })
+
+    this.addTrackNotifier$.subscribe(({ mediaStream, track }) => {
+      this.trackList$.next([...this.trackList$.value, track])
+
+      fromEvent(track, 'ended')
+        .pipe(take(1))
+        .subscribe(() => {
+          this.removeTrackNotifier$.next({ mediaStream, track })
+
+          if (mediaStream.getTracks().length === 0) {
+            this.removeMediaStreamNotifier$.next(mediaStream)
+          }
+        })
+    })
+
+    this.removeTrackNotifier$.subscribe(({ mediaStream, track }) => {
+      mediaStream.removeTrack(track)
+      this.trackList$.next(
+        this.trackList$.value.filter((t) => t.id !== track.id),
+      )
+    })
+  }
+
+  static isVideoEnabled(mediaStream: MediaStream) {
+    if (mediaStream.getVideoTracks().length <= 0) {
+      return false
+    }
+    return mediaStream.getVideoTracks().some((videoTrack) => videoTrack.enabled)
+  }
+
+  static isAudioEnabled(mediaStream: MediaStream) {
+    if (mediaStream.getAudioTracks().length <= 0) {
+      return false
+    }
+    return mediaStream.getAudioTracks().some((audioTrack) => audioTrack.enabled)
+  }
+
+  private getMediaStream(id: string) {
+    return this.mediaStreamList$.value.find((ms) => ms.id === id)
+  }
+
+  private addUserMediaStreamTrack$(
+    mediaStream: MediaStream,
+    constraints: MediaStreamConstraints,
+  ) {
+    return from(window.navigator.mediaDevices.getUserMedia(constraints)).pipe(
+      map((ms) => ms.getTracks()),
+      tap((tracks) => {
+        tracks.forEach((track) => {
+          mediaStream.addTrack(track)
+          this.addTrackNotifier$.next({ mediaStream, track })
+        })
+      }),
+    )
+  }
+
+  private upsertUserMediaStreamTrack$(
+    mediaStream: MediaStream,
+    constraints: MediaStreamConstraints,
+  ) {
+    const upsertVideoMediaStreamTrack$ = constraints.video
+      ? of(mediaStream.getVideoTracks()).pipe(
+          switchMap((videoTracks) => {
+            if (videoTracks.length > 0) {
+              return of(videoTracks)
+            }
+            return this.addUserMediaStreamTrack$(mediaStream, {
+              video: constraints.video,
+            })
+          }),
+        )
+      : of([])
+    const upsertAudioMediaStreamTrack$ = constraints.audio
+      ? of(mediaStream.getAudioTracks()).pipe(
+          switchMap((audioTracks) => {
+            if (audioTracks.length > 0) {
+              return of(audioTracks)
+            }
+            return this.addUserMediaStreamTrack$(mediaStream, {
+              audio: constraints.audio,
+            })
+          }),
+        )
+      : of([])
+
+    return forkJoin([
+      upsertVideoMediaStreamTrack$,
+      upsertAudioMediaStreamTrack$,
+    ]).pipe(
+      map(([videoTracks, audioTracks]) => [...videoTracks, ...audioTracks]),
+    )
+  }
+
+  private setTrackEnabled$(
+    enabled: boolean,
+    mediaStream: MediaStream,
+    constraints: MediaStreamConstraints,
+  ) {
+    return this.upsertUserMediaStreamTrack$(mediaStream, constraints).pipe(
+      tap((tracks) => {
+        for (let i = 0; i < tracks.length; i += 1) {
+          const track = tracks[i]
+          if (track) {
+            track.enabled = enabled
+          }
+        }
+      }),
+      map(() => mediaStream),
+    )
+  }
+
+  setVideoEnabled$(
+    enabled: boolean,
+    mediaStream: MediaStream,
+    constraints: Pick<MediaStreamConstraints, 'video'> = { video: true },
+  ) {
+    return of(this.getMediaStream(mediaStream.id)).pipe(
+      filter((ms): ms is MediaStream => !!ms),
+      switchMap((ms) => this.setTrackEnabled$(enabled, ms, constraints)),
+      tap((ms) => {
+        this.enableTrackNotifier$.next(ms)
+      }),
+    )
+  }
+
+  setAudioEnabled$(
+    enabled: boolean,
+    mediaStream: MediaStream,
+    constraints: Pick<MediaStreamConstraints, 'audio'> = { audio: true },
+  ) {
+    return of(this.getMediaStream(mediaStream.id)).pipe(
+      filter((ms): ms is MediaStream => !!ms),
+      switchMap((ms) => this.setTrackEnabled$(enabled, ms, constraints)),
+      tap((ms) => {
+        this.enableTrackNotifier$.next(ms)
+      }),
+    )
+  }
+
+  observeTrackEnabled$(mediaStream: MediaStream) {
+    return of(this.getMediaStream(mediaStream.id)).pipe(
+      filter((ms): ms is MediaStream => !!ms),
+      switchMap((ms) => merge(of(ms), this.enableTrackNotifier$)),
+      filter((ms) => ms.id === mediaStream.id),
+      map((ms) => ({
+        video: LocalParticipant.isVideoEnabled(ms),
+        audio: LocalParticipant.isAudioEnabled(ms),
+      })),
+      takeUntil(
+        this.removeMediaStreamNotifier$.pipe(
+          filter((ms) => ms.id === mediaStream.id),
+        ),
+      ),
+    )
+  }
+
+  clear() {
+    this.mediaStreamList$.value.forEach((mediaStream) => {
+      this.removeMediaStreamNotifier$.next(mediaStream)
+    })
+    this.mediaStreamList$.complete()
+    this.trackList$.complete()
+    this.addMediaStreamNotifier$.complete()
+    this.removeMediaStreamNotifier$.complete()
+    this.addTrackNotifier$.complete()
+    this.removeTrackNotifier$.complete()
+    this.enableTrackNotifier$.complete()
+  }
+}
+
+export default LocalParticipant
